fb7b59b62c4a20fa64eecb0e59d747b61a9935ed
101Amine <aminebecha8@gmail.com>
2022-09-16 12:45:59 +0100

    ..

--- src/Components/CommentElement.js
+++ src/Components/CommentElement.js
@@ -11,9 +11,9 @@ export const CommentElement = ({img, user, date, comment}) => {
     <article>
       
         <div className='likes_count'>
-            <div> <img src='./images/icon-plus.svg' alt='icon_plus' /> </div>
+            <div> <img className='clickable' src='./images/icon-plus.svg' alt='icon_plus' /> </div>
             <div> 12 </div>
-            <div> <img src='./images/icon-minus.svg' alt='icon_minus' /></div>
+            <div> <img className='clickable'  src='./images/icon-minus.svg' alt='icon_minus' /></div>
         </div>
 
 
@@ -30,8 +30,16 @@ export const CommentElement = ({img, user, date, comment}) => {
                 <div className='right_side'>
                     <img className='icon_reply' src='./images/icon-reply.svg' alt='icon_reply'/>
 
-                    <button className='btn' onClick={handleReply} > Reply </button>
+                    <button className='btn clickable' onClick={handleReply} > Reply </button>
                 </div>
+            </div>
+
+            <div className='bottom_side'>
+                  <p>
+                    Impressive! though it seems the drag feature could be improved, But overall it looks incredible, You've nailed the design and the responsiveness at various breakpoints works really well. 
+                  </p>
+
+
             </div>
             
         </div>
